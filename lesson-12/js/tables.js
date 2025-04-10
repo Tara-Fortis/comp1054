e68a2ba2e917d4f01f1b276ea213291758ce9121
Tineil_A <tine6573@gmail.com>
2025-04-10 15:02:57 -0400

    completed table css and added tablesort.js

--- lesson-12/js/tables.js
+++ lesson-12/js/tables.js
@@ -1,1 +1,5 @@
 // STEP 11: Call to tablesorter script here
+// grab the table
+const consolesTable = document.querySelector("table");
+// enable TableSort.js on the table
+let myTable = new Tablesort(consolesTable);
